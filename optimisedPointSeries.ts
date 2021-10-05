d0a6f5eec4159595cc74262cc470c4c44c69709c
Chris Price <cprice@scottlogic.co.uk>
2021-10-05 14:43:25 +0100

    rename and simplify to optimised point series

--- optimisedPointSeries.ts
+++ optimisedPointSeries.ts
@@ -1,39 +1,26 @@
-import { scaleIdentity, symbolCircle } from 'd3';
+import { scaleIdentity } from 'd3';
 import {
     rebind,
     webglSeriesPoint,
-    webglScaleMapper,
-    webglSymbolMapper
+    webglScaleMapper
 } from 'd3fc';
 import webglConstantAttribute from '@d3fc/d3fc-webgl/src/buffer/constantAttribute'
-import streamingAttribute from './streamingAttribute'
 
-export default (maxByteLength) => {
-    const crossValueAttribute = streamingAttribute()
-        .maxByteLength(maxByteLength);
-    const mainValueAttribute = streamingAttribute()
-        .maxByteLength(maxByteLength);
+export default () => {
     const sizeAttribute = webglConstantAttribute();
     const definedAttribute = webglConstantAttribute();
 
     const draw = webglSeriesPoint()
-        .crossValueAttribute(crossValueAttribute)
-        .mainValueAttribute(mainValueAttribute)
         .sizeAttribute(sizeAttribute)
         .definedAttribute(definedAttribute);
 
     let xScale = scaleIdentity();
     let yScale = scaleIdentity();
-    let mainValues = d => d.y;
     let decorate = (programBuilder, data, index) => { };
-    let crossValues = d => d.x;
 
     const streamingPointSeries = (data) => {
         sizeAttribute.value([1]);
         definedAttribute.value([true]);
-
-        mainValueAttribute.data(mainValues(data));
-        crossValueAttribute.data(crossValues(data));
 
         // The following assumes there is no d3 scale required
         const xWebglScale = webglScaleMapper(xScale).webglScale;
@@ -41,10 +28,10 @@ export default (maxByteLength) => {
 
         draw.xScale(xWebglScale)
             .yScale(yWebglScale)
-            .type(webglSymbolMapper(symbolCircle))
             .decorate(programBuilder => {
                 decorate(programBuilder, data, 0);
             });
+
         draw(data.length);
     };
 
@@ -64,22 +51,6 @@ export default (maxByteLength) => {
         return streamingPointSeries;
     };
 
-    streamingPointSeries.mainValues = (...args) => {
-        if (!args.length) {
-            return mainValues;
-        }
-        mainValues = args[0];
-        return streamingPointSeries;
-    };
-
-    streamingPointSeries.crossValues = (...args) => {
-        if (!args.length) {
-            return crossValues;
-        }
-        crossValues = args[0];
-        return streamingPointSeries;
-    };
-
     streamingPointSeries.decorate = (...args) => {
         if (!args.length) {
             return decorate;
@@ -88,7 +59,7 @@ export default (maxByteLength) => {
         return streamingPointSeries;
     };
 
-    rebind(streamingPointSeries, draw, 'context', 'pixelRatio');
+    rebind(streamingPointSeries, draw, 'context', 'pixelRatio', 'type', 'mainValueAttribute', 'crossValueAttribute');
 
     return streamingPointSeries;
 };
